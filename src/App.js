bc18e6fb9055c71ca0e1388e608dd0a0e18aec8b
Lauri Paronen <lauri.m.paronen@tuni.fi>
2023-04-27 00:10:43 +0300

    start working on Header.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header/Header';
 
 function App() {
-  // 
+  return (
+    <div className="App">
+      <Header />
+      {/* Add the rest of your components here */}
+    </div>
+  );
 }
 
 export default App;
