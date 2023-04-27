9c796d2abdc4fcb4c82d703332bccae30a78d634
Lauri Paronen <lauri.m.paronen@tuni.fi>
2023-04-27 18:28:31 +0300

    add second book to bookshelf w desc

--- src/components/Bookshelf/Bookshelf.js
+++ src/components/Bookshelf/Bookshelf.js
@@ -15,7 +15,11 @@ function Bookshelf() {
         </p>
         <div className="bookshelf-list">
           <div className="bookshelf-list-item">
-            <h3 className="bookshelf-list-item-title">Tao Te Ching</h3>
+            <h3 className="bookshelf-list-item-title">
+              <a href="https://www.with.org/tao_te_ching_en.pdf"
+              target="_blank"
+              >Tao Te Ching
+              </a></h3>
             <p className="bookshelf-list-item-author">Laozi</p>
             <p className="bookshelf-list-item-desc">
               This is a quintessential book. No other work of literature
@@ -37,10 +41,31 @@ function Bookshelf() {
             </p>
           </div>
           <div className="bookshelf-list-item">
-            <h3 className="bookshelf-list-item-title">kirjan nimi 2</h3>
-            <p className="bookshelf-list-item-author">homokulli</p>
+            <h3 className="bookshelf-list-item-title">
+              <a 
+              href="https://en.wikisource.org/wiki/Fragments_of_Heraclitus"
+              target="_blank">
+                Fragments
+                </a></h3>
+            <p className="bookshelf-list-item-author">Heraclitus</p>
             <p className="bookshelf-list-item-desc">
-
+              This obscure work, assembled from whatever was left of 
+              the intellectual output of the Ancient Greek philosopher
+              Heraclitus, is similar to the Tao Te Ching in many respects.
+              Heraclitus is my favorite philosopher of the Western canon for
+              exactly this reason; the constrained and random nature of the
+              fragments that remain of his work force the reader to 
+              contemplate, drawing novel interpretations not so much from the 
+              text as from their own mind. <br /><br />
+              Heraclitus is also one of the few Western philosophers who
+              apprehended the <i>unity of opposites</i>. This is a concept
+              discovered by the Ancient Chinese, notoriously represented in the
+              yin-yang symbol. It is a concept that is so essential and fundamental
+              to the nature of reality that it escapes most of us. Alan Watts,
+              my favorite philosopher of the 20th century, called it the only 
+              metaphysical lesson one needs to learn: that all fronts
+              come with backs, and all ups come with downs. Quoting
+              Heraclitus' 60th fragment, "The way up and the way down is one and the same."  <br /><br />
             </p>
           </div>
           </div>
