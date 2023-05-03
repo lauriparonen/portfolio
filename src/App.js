c448d3fd02d88467d33a55f7920b20538f2c1852
Lauri Paronen <lauri.m.paronen@tuni.fi>
2023-05-03 18:09:41 +0300

    add todo to App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
 import { useLocation } from 'react-router-dom';
 
+// todo: adjust site to be mobile friendly;
+// finish gallery page;
+// ensure all components work as intended on mobile and all browsers
+
 
 import './App.css';
 import Metaphysics from './components/Metaphysics/Metaphysics';
